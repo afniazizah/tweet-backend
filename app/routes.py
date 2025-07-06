635f9bdda39abf00d6dee8c6ad1e9ca7499bf977
Ipan Badruzzaman <ipannbzn1@gmail.com>
2025-07-06 14:48:42 +0700

    fix bug and error fase 1

--- app/routes.py
+++ app/routes.py
@@ -16,14 +16,20 @@ def get_image(filename):
 def upload_file():
     print(request)
     if 'file' not in request.files:
-        return jsonify({"error": "No file part in the request"}), 400
-
+        return jsonify({"error": "Error: Tidak ada file yang diupload"}), 42
     file = request.files['file']
     table_name = request.form.get('table_name')
-    df = pd.read_csv(file)
-    if table_name not in df.columns:
-        return jsonify({"error": f"Kolom '{table_name}' tidak ditemukan dalam file"}), 422
-    df.rename(columns={table_name: 'full_text'})
-    filename = str(uuid.uuid4()) + os.path.splitext(file.filename)[1]
-    df.to_csv(TWEETS_DATA_PATH + '/' + filename, index=False)
-    return jsonify({"filename": filename}), 200
+    try:
+        df = pd.read_csv(file)
+        if table_name not in df.columns:
+            return jsonify({"error": f"Kolom '{table_name}' tidak ditemukan dalam file"}), 422
+        print(df[table_name].head())
+        if len(df[table_name]) < 100:
+            return jsonify({"error": f"Data harus memiliki minimal 100 baris data"}), 422
+        df.rename(columns={table_name: 'full_text'})
+        filename = str(uuid.uuid4()) + os.path.splitext(file.filename)[1]
+        df.to_csv(TWEETS_DATA_PATH + '/' + filename, index=False)
+        return jsonify({"filename": filename}), 200
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": "Error: File yang diupload tidak valid"}), 422
